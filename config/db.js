fa1bea2e95729c9bc3b1c471275f09bba49111d3
Mateusz Derus <mateuszderus01@gmail.com>
2022-07-14 23:48:35 +0200

    env quick fix in db

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(
-  'js_shop',
+  process.env.MYSQL_DB,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
