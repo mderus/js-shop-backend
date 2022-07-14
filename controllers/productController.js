b2edd61b645de131b12787656302dcc5b22ae589
Mateusz Derus <mateuszderus01@gmail.com>
2022-07-15 01:05:30 +0200

    productController added and updated routes

---
+++ controllers/productController.js
@@ -0,0 +1,29 @@
+const asyncHandler = require('express-async-handler');
+const Product = require('../models/Product');
+
+// Fetch all products
+// GET /api/products
+// Public
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.findAll(req.params.products);
+  res.json(products);
+});
+
+// Fetch product by id
+// GET /api/products/:id
+// Public
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findOne({where: {_id: req.params.id}});
+
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error('Product not found');
+  }
+});
+
+module.exports = {
+  getProducts,
+  getProductById,
+};
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,29 +1,12 @@
 const express = require('express');
-const asyncHandler = require('express-async-handler');
-const Product = require('../models/Product');
-
 const productRouter = express.Router();
 
-productRouter
-  .get(
-    '/',
-    asyncHandler(async (req, res) => {
-      const products = await Product.findAll(req.params.products);
-      res.json(products);
-    })
-  )
-  .get(
-    '/:id',
-    asyncHandler(async (req, res) => {
-      const product = await Product.findOne({where: {_id: req.params.id}});
+const {
+  getProducts,
+  getProductById,
+} = require('../controllers/productController');
 
-      if (product) {
-        res.json(product);
-      } else {
-        res.status(404);
-        throw new Error('Product not found');
-      }
-    })
-  );
+productRouter.route('/').get(getProducts);
+productRouter.route('/:id').get(getProductById);
 
 module.exports = {productRouter};
