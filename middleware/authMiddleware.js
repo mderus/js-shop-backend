1fc1468a0de2f36b66dba2a1160cbd219303c7c7
Mateusz Derus <mateuszderus01@gmail.com>
2022-07-18 14:49:40 +0200

    auth middleware update

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -12,8 +12,7 @@ const protect = asyncHandler(async (req, res, next) => {
     try {
       token = req.headers.authorization.split(' ')[1];
       const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-      // @TODO: find out how to cut a piece of data (password) from this returned promise
-      req.user = await User.findOne({where: {_id: decodedToken.id}});
+      req.user = await User.findById(decodedToken.id).select(-password);
 
       next();
     } catch (error) {
